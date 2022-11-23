edcdf6b8b989acdf9b88022bbbf03466b86add32
Nathaniel3131 <97564036+Nathaniel3131@users.noreply.github.com>
2022-11-23 02:15:20 +0200

    Delete Microwave_Facade directory

--- Microwave_Facade/src/com/company/ControlPanel.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class ControlPanel {
-    public static byte[] point(long menuPoint, int time) {
-        System.out.println("\nПункт меню = " + menuPoint);
-        System.out.println("Час = " + time + "хв.");
-        return new byte[time];
-    }
-}
--- Microwave_Facade/src/com/company/GuideRoller.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class GuideRoller {
-    public void load(long menu, byte[] point) {
-        System.out.println("\nПочаток виконання пункту меню " + menu + " - Зажарка");
-    }
-}
